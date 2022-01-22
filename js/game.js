05fca5f807f34607cf6ff1e2ef0d40f88691719e
Hibaber <hiba.berber@gmail.com>
2022-01-22 19:49:53 +0100

    second commit

--- js/game.js
+++ js/game.js
@@ -5,22 +5,108 @@ appName: 'The IronRat Journey app',
     license: undefined,
     gameSize: { w: undefined, h: undefined },
     ctx: undefined,
+    canvas: undefined,
+    FPS: 60,
+    framesCounter: 0,
+    background: undefined,
+    rat: undefined,
+    enemies: [], // crear un array por cada tipo de enemigo o un único array?
+    goals: [], // idem
+    prizes:[], // idem
+    keys: {
+        // TOP: , // añadir número
+        // SPACE: , // añadir número
+    },
+
+
     init() {
-        this.setContext()
-        this.setSize()
+        this.canvas = document.getElementById('canvas')
+        this.ctx =  this.canvas.getContext('2d')
+        this.setDimensions()
+        this.setEventListeners()
+        this.start()
     },
-    setContext() {
-        this.ctx = document.querySelector('#canvas').getContext('2d')
-        console.log(this.ctx)
+ 
+    setDimensions(){
+        this.gameSize.w = window.innerWidth,
+        this.gameSize.h = window.innerHeight
+        this.canvas.setAttribute('width', this.gameSize.w)
+        this.canvas.setAttribute('width', this.gameSize.h)
     },
-    setSize() {
-        this.gameSize = {
-            w: window.innerWidth,
-            h: window.innerHeight
-        }
-        document.querySelector('#canvas').setAttribute('width', this.gameSize.w)
-        document.querySelector('#canvas').setAttribute('height', this.gameSize.h)
+
+    setEventListeners() {
+        document.addEventListener('keydown', e => {
+            // switch (e.key) {
+            //     case this.keys.TOP:
+            //         ...
+            // }
+        })
     },
+
+    start() {
+
+        this.reset()
+        // this.interval = setInterval(()=>{
+
+        //     this.framecounter > 5000 ? this.framesCounter = 0: this.framesCounter++
+
+        //     // this.createEnemies()
+
+        //     // this.createPrizes()
+
+        //     // this.createGoals()
+
+        //     // this.clearEnemies()
+
+        //     // this.isColission() ? this.gameOver() : null
+
+        //     this.clearAll()
+        //     this.drawAll()
+        // }, 1000 / this.FPS)
+    },
+
+    reset() {
+        this.background = new Background(this.ctx, this.gameSize.w, this.gameSize.h, '#') // aqui va el enlace de una imagen
+        this.rat = new Rat(this.ctx, this.gameSize.w, this.gameSize.h, this.keys) 
+        this.enemies = [], 
+        this.prizes = [], 
+        this.goals = []
+        
+    },
+
+    drawAll() {
+        this.background.draw()
+        this.rat.draw(this.framesCounter)
+        this.enemies.forEach(element => element.draw())
+        this.prizes.forEach(element => element.draw())
+        this.goals.forEach(element => element.draw())
+        
+    },
+
+    clearAll() {
+        this.ctx.clearRect(0, 0, this.gameSize.w, this.gameSize.h)
+    },
+
+    createEnemies() {
+        // if (this.framesCounter % 90 === 0){
+        //     this.enemies.push(new Enemies(this.ctx, this.gameSize.w, this...))
+        // }
+    }
+
+// clearEnemies
+
+// isColission (tenemos dos colisiones)
+
+// gameOver
+
+    
+
+
+
+
+    
+
+ 
 
 
 
---
+++ js/player.js
@@ -0,0 +1,8 @@
+class Rat {
+  constructor(ctx, gameW, gameH, keys) {
+    this.ctx = ctx;
+    this.gameSize.h = gameH;
+    this.gameSize.h = gameH;
+    this.ratSize = { w: 100, h: 100 };
+  }
+}
