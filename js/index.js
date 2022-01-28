ed8ae96244595e34c6132f36f0e4946099175b77
Ernesto Espinoza M <emenocal07@gmail.com>
2022-01-28 11:13:46 +0100

    Co-authored-by: Hibaber <Hibaber@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let button = document.querySelector('#button')
 let toBeRemoved = document.querySelectorAll(".hide")
 
 const startGame = () => {
-    console.log('hola')
     button.addEventListener('click', (e) => {
         toBeRemoved.forEach((elem) => { elem.remove(e) })
         game.init()
